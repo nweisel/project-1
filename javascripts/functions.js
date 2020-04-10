79cb3ace3d071e0493b5022af0f2c6aaa9bf8973
Noah Weisel <nweisel@uoregon.edu>
2020-04-10 12:33:37 -0700

    Adde sumEvens to functions.js

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //jshint esversion: 6
 
 let arrAvg = (arr) => {
@@ -18,24 +17,13 @@ let arrMax = (numbers) => {
   });
   return currentMaximum;
 };
-=======
-//jshint esversion: 6
 
-let arrAvg = (arr) => {
+let sumEvens = (numbers) => {
   let sum = 0;
-  for (var i = 0; i < arr.length; i++) {
-    sum = sum + arr[i];
-  }
-  return sum / arr.length;
-};
-
-let arrMax = (numbers) => {
-  let currentMaximum = -Inifinity;
   numbers.forEach(function(number){
-    if (number > currentMaximum) {
-      currentMaximum = number;
+    if (number % 2 == 0){
+      sum += number;
     }
   });
-  return currentMaximum;
-}
->>>>>>> 0c35d4cfc9262f6c3b4ab119d94eaf74c4f39bd2
+  return sum;
+};
